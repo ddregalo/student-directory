dd1460f5e3f082efa2dc43b8ecbe1f29a152bf41
danielcampos <ddregalo@gmail.com>
2018-02-13 14:05:56 +0000

    Extension exercise 3: Print names with less than 12 characters

--- directory.rb
+++ directory.rb
@@ -18,21 +18,28 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of Villains Academy with names smaller than 12 characters"
   puts "-------------"
+end
+
+def student_count(students)
+  students.select { |student| student[:name].length < 12}.count
 end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].length < 12
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students} great students"
 end
 students = input_students
+student_count = student_count(students)
 #nothing happens until we call the methods
 print_header
 print(students)
-print_footer(students)
+print_footer(student_count)
