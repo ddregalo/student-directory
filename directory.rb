178ea487428a38e170df846425e9d125d39954d6
danielcampos <ddregalo@gmail.com>
2018-02-13 18:42:31 +0000

    Extension exercise 7: Add cohort input with default and typo control

--- directory.rb
+++ directory.rb
@@ -5,13 +5,54 @@ def input_students
   students = []
   #get the first name
   name = gets.chomp
+  #get the cohort
+  puts "What cohort is the student in"
+  cohort = gets.chomp.strip.downcase.to_sym
+  cohort_groups = [
+    :january,
+    :february,
+    :march,
+    :april,
+    :may,
+    :june,
+    :july,
+    :august,
+    :september,
+    :october,
+    :november,
+    :december
+  ]
+  while true do
+    if cohort == ""
+      cohort = "december".to_sym
+    elsif cohort_groups.include?(cohort)
+      break
+    else
+      puts "Re-enter cohort"
+      cohort = gets.chomp.strip.downcase.to_sym
+    end
+  end
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort}
     puts  "Now we have #{students.count} students"
     # get another name from the user
+    puts "Next student name"
     name = gets.chomp
+    break if name == ""
+    puts "What cohort is the student in"
+    cohort = gets.chomp.strip.downcase.to_sym
+    while true do
+      if cohort == ""
+        cohort = "december".to_sym
+      elsif cohort_groups.include?(cohort)
+        break
+      else
+        puts "Re-enter cohort"
+        cohort = gets.chomp.strip.downcase.to_sym
+      end
+    end
   end
   # return the array of students
   students
@@ -24,7 +65,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
   end
 end
 
