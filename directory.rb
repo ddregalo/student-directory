26510c93edb50ca05337c195cfa18f4fc7658368
danielcampos <ddregalo@gmail.com>
2018-02-14 21:42:19 +0000

    Extension exercise 10: Use alternate chomp method

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets[0..-2]
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts  "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets[0..-2]
   end
   # return the array of students
   students
