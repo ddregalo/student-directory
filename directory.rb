f5ec51b99e34df76f892cf9268cb357c1ad5e083
danielcampos <ddregalo@gmail.com>
2018-02-16 01:16:42 +0000

    Refactor: Shorten interactive menu by creating methods

--- directory.rb
+++ directory.rb
@@ -1,20 +1,19 @@
+@students = []
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts  "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts  "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
 def print_header
@@ -22,41 +21,50 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  if @students.count >= 1
+    print_header
+    print_students_list
+    print_footer
+  else puts "There are no students"
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      # input the students
+      @students = input_students
+    when "2"
+      # show the students
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
-  students = []
   loop do
-    #print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    selection = gets.chomp
-    case selection
-      when "1"
-        # input the students
-        students = input_students
-      when "2"
-        # show the students
-        if students.count >= 1
-          print_header
-          print(students)
-          print_footer(students)
-        else puts "There are no students"
-        end
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
