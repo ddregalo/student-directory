44d037c46a142db2fcf1cd9cbb50eac9be80938d
danielcampos <ddregalo@gmail.com>
2018-02-16 00:12:21 +0000

    Extension exercise 12: Only print students if count greater than 1

--- typos.rb
+++ typos.rb
@@ -1,15 +1,15 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
+  # create an empty array
   students = []
-  #get the first name
+  # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts  "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
@@ -18,19 +18,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_students(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 students = input_students
 if students.count >= 1
   print_header
